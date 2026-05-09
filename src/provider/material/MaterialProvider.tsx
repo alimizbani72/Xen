48ba6beffa1aa9d36f92318c8513cb10e2e7c502
alimizbani72 <alimizbani88887@gmail.com.com>
2026-05-09 10:53:16 +0330

    fix eslint war

--- src/provider/material/MaterialProvider.tsx
+++ src/provider/material/MaterialProvider.tsx
@@ -1,26 +1,35 @@
 'use client'
 import { CssBaseline } from '@mui/material'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { FC, PropsWithChildren, useMemo } from 'react'
 import mediaQuery from 'css-mediaquery'
+import { FC, PropsWithChildren, useCallback, useMemo } from 'react'
 
 import { themeOptions } from './theme'
 
 const MaterialProvider: FC<PropsWithChildren<{ deviceType?: string }>> = ({ children, deviceType }) => {
-  const ssrMatchMedia = (query: string) => ({
-    matches: mediaQuery.match(query, {
-      width: deviceType === 'mobile' ? '0px' : '1024px',
+  const ssrMatchMedia = useCallback(
+    (query: string) => ({
+      matches: mediaQuery.match(query, {
+        width: deviceType === 'mobile' ? '0px' : '1024px',
+      }),
     }),
-  })
+    [deviceType],
+  )
 
   const usedTheme = useMemo(
     () =>
       createTheme({
         ...themeOptions,
-        components: { ...themeOptions.components, MuiUseMediaQuery: { defaultProps: { ssrMatchMedia } } },
+        components: {
+          ...themeOptions.components,
+          MuiUseMediaQuery: {
+            defaultProps: { ssrMatchMedia },
+          },
+        },
       }),
-    [],
+    [ssrMatchMedia],
   )
+
   return (
     <ThemeProvider theme={usedTheme}>
       <CssBaseline />
